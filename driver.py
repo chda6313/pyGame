42ed9a633d5f0631bdbba377b41817688a185bd4
Picklwarrior <ntnsbtaylor@gmail.com>
2017-02-15 20:01:22 -0500

    insignificant

--- driver.py
+++ driver.py
@@ -5,7 +5,7 @@ def __main__():
 	#make a ship
 	PlayerShip = classes.ship()
 	#it has 500 money
-	PlayerShip.money = 600
+	PlayerShip.money = 500
 	#Let's save that
 	qolFunctions.saveGame(PlayerShip)
 	#Oh no, we lost 200 money
@@ -13,6 +13,6 @@ def __main__():
 	#so let's load our last save
 	PlayerShip = qolFunctions.loadGame()
 	#Oh good, it's all here
-	print(str(PlayerShip.money))
+	print(str(PlayerShip.armor))
 
 __main__()
