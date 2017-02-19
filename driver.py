357b0c615c971f5fba4e4f62eb05fcf92832799c
Picklwarrior <ntnsbtaylor@gmail.com>
2017-02-18 23:24:56 -0500

    misc

--- driver.py
+++ driver.py
@@ -3,10 +3,6 @@ import os, sys, random, classes, qolFunctions
 
 def __main__():
 
-
-##################################################HEY CHARLIE using money instead of fuel breaks the load function for some reason
-##################################################Works with fuel but not money. Weird.
-
 #make a ship
 	PlayerShip = classes.ship()
 	PlayerShip.name = "Serenity"
@@ -19,8 +15,7 @@ def __main__():
 #load save file
 	PlayerShip = qolFunctions.loadGame("Serenity_Save.txt")
 #did it work?
-	print(PlayerShip.fuel)
-
+	print(PlayerShip.money)
 
 #This will create a random male name
 	#name = qolFunctions.maleNameGen()
@@ -31,7 +26,6 @@ def __main__():
 	#name = qolFunctions.femaleNameGen()
 	#print("And here is a female one:")
 	#print(name,"\n")
-
 
 #Playing with levelCreator
 	#qolFunctions.levelCreator("Level1.txt")
